38fc40fbbe3ec43256ba7e1312d67962738948e5
tayyab-tahir-dev <tt0370260@gmail.com>
2026-08-17 14:21:32 -0900

    Learn INTERPRETER

---
+++ PYTHON_FUNDAMENTALS.py
@@ -0,0 +1,67 @@
+# What is PYTHON?
+# PYTHON is a high-level programming language used to give instructions to computers and build 
+# different types of applications, and softwares etc.  
+
+
+# What is REPL?
+# REPL stands for Read, Eval, Print, Loop. It is an interactive programming environment that
+# takes single user inputs (i.e. single expressions), evaluates them, and returns the result to 
+# the user. The REPL environment is a great way to test out code snippets and see immediate 
+# results.
+
+# SIMPLE EXAMPLE OF REPL IS:
+# REPL mai hum apna code line-by-line likhte hai aur uska output immediately dekhta hai.For 
+# texting
+
+
+
+                   # <--------INTERPRETER IN PYTHON----------->
+# A Python interpreter is a program that reads and executes Python code so your computer can run 
+# it.
+
+# Hum PYTHON code likhtay hai 
+
+# print("Hello World")
+
+# Python interpreter is code ko bytecode mein convert karta hai.
+# Python Virtual Machine (PVM) bytecode ko read karti hai.
+# PVM is bytecode ki instructions ko execute karwati hai.
+# Aakhir mein CPU task perform karta hai or result show krvata hai.
+
+# CPU khud screen par result show nahi karta. CPU calculation/operations karta hai, phir 
+# operating system + terminal/display system result ko screen par show karte hain.
+
+
+
+                    # <--------NAMING RULE IN PYTHON----------->
+
+# NAMING RULE mtlb ki hum variable ka naam kaise rakh sakte hai.Inkay kuch rules hai jo hume 
+# follow karnay parta hai.
+
+# EXAMPLE 
+
+# 1) Naam letter ya underscore say start hona chahiye.
+# 2) Naam may letter,numbers or underscore use kr sktay hai.
+# 3) Naam may space use nhi kr sktay.
+
+
+                      #  <------------VARIABLES---------------->
+
+
+
+# Variables are used to store and name data.
+# Variables python mai ek container ki tarah kaam karte hai jisme hum data ko store kar sakte hai
+# aur uska naam de sakte hai.Or time ke sath hum us data ko change bhi kar sakte hai.
+
+# PYTHON mai JS ki tarah variable declare karne ke liye 'var','let' ya 'const' ka use nhi karte.
+
+# print() function console.log() ki tarah kaam karta hai.
+
+# How to create a variable in PYTHON?
+
+# name = "Tayyab"
+# print(name)
+
+# name is the variable.
+# = mtlb store the value in the variable.
+# "Tayyab" is the value of that variable.
