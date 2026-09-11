38334ab3be1be54cf3690a771f5b707770780245
tayyab-tahir-dev <tt0370260@gmail.com>
2026-09-11 14:43:31 -0900

    LEARN BASICS OF FUNCTION

--- PHASE3:FUNCTIONS.py
+++ PHASE3:FUNCTIONS.py
@@ -13,3 +13,56 @@
 # 9) Closures
 # 10) Decorators — Introduction
 # 11) Real AI Examples
+
+# - FUNCTION--->
+
+# Definition:
+# Function: A function is a reusable block of code that performs a specific task when it is called.
+
+# SIMPLE UNDERSTAND:
+# Function code ka reusable block hota hai jo ek specific kaam karta hai jab usay call kiya jata 
+# hai.
+
+# EXAMPLES:
+
+# 1)-
+
+# def greet():
+#     print("Hello World")
+
+
+# def     → function banane ka keyword
+# greet   → function ka naam
+# ()      → parameters/arguments ki jagah/abhi empty hain
+# :       → function body start hone ka signal
+# print() → function ka kaam
+# greet() → function ko call karne ka tarika
+
+# def greet() function ka woh hissa hai jahan hum function ko define karte hain aur uska naam 
+# batate hain.
+
+# print("Hello World")
+# yeh function body hai.
+
+# Simple rule:
+# : se pehle = function ki heading
+# : ke baad indented code = function ki body
+
+# Define = function banana
+# Call = function ko chalana
+
+# 2)-
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Tayyab")
+
+# 3)-
+
+# def welcome():
+#     print("Learning Python")
+
+# welcome()
+# welcome()
+# welcome()
